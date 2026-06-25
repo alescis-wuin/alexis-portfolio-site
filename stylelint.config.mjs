f316c70b1bf126d8a6119f5f293b83b3e81565c5
Alexis <alexis.guinot@onsiea.com>
2026-06-25 09:08:51 +0200

    Add Stylelint configuration

---
+++ stylelint.config.mjs
@@ -0,0 +1,35 @@
+export default {
+  ignoreFiles: [
+    'node_modules/**',
+    'dist/**',
+    'build/**',
+    'coverage/**',
+    'playwright-report/**',
+    'test-results/**'
+  ],
+  rules: {
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'declaration-block-no-duplicate-properties': [true, {
+      ignore: ['consecutive-duplicates-with-different-values']
+    }],
+    'font-family-no-duplicate-names': true,
+    'function-linear-gradient-no-nonstandard-direction': true,
+    'keyframe-block-no-duplicate-selectors': true,
+    'media-feature-name-no-unknown': true,
+    'named-grid-areas-no-invalid': true,
+    'no-descending-specificity': null,
+    'no-duplicate-at-import-rules': true,
+    'no-duplicate-selectors': true,
+    'no-invalid-double-slash-comments': true,
+    'property-no-unknown': [true, {
+      ignoreProperties: ['scrollbar-gutter']
+    }],
+    'selector-pseudo-class-no-unknown': [true, {
+      ignorePseudoClasses: ['where', 'is', 'has']
+    }],
+    'selector-pseudo-element-no-unknown': true,
+    'string-no-newline': true,
+    'unit-no-unknown': true
+  }
+};
